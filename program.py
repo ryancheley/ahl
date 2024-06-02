1946e44e80724da8f1dba852251448c647114af3
Ryan Cheley <rcheley@gmail.com>
2024-06-02 08:10:00 -0700

    🐛 Fixed a parsing bug

--- program.py
+++ program.py
@@ -5,6 +5,26 @@ import time
 
 import requests
 
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Agent": "Ryan",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Cookie": "PHPSESSID=7po31mvdjpu7mj978ft8hssqod",
+    "Host": "lscluster.hockeytech.com",
+    "Sec-Ch-Ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "macOS",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+}
+
 
 def get_team(game_details, home_or_away):
     if home_or_away == "home":
@@ -66,7 +86,7 @@ def get_game_details(response: str):
         line_item = i.strip()
         try:
             start_item = line_item[0]
-            if start_item != "<":
+            if start_item != "<" and start_item != "(":
                 game_details = []
                 page_details = line_item.split("<br />")
                 for item in page_details:
