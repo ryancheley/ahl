711461418ce066a045b18a0c68910bceb64a4556
Ryan Cheley <rcheley@gmail.com>
2025-11-30 16:25:01 -0800

    🔧 configure settings for coolify

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['http://localhost:8000'])
 
 # Application definition
 
